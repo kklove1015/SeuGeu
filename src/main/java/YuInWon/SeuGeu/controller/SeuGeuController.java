d1692d521c27c1db0c6424015353428a9ff9a1fe
kklove1015 <kklove1015@gmail.com>
2022-03-17 13:52:54 +0900

    test domain join

--- src/main/java/YuInWon/SeuGeu/controller/SeuGeuController.java
+++ src/main/java/YuInWon/SeuGeu/controller/SeuGeuController.java
@@ -1,4 +1,15 @@
 package YuInWon.SeuGeu.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SeuGeuController {
+
+    @GetMapping("test")
+    public String Test() {
+        return "test";
+    }
+
 }
